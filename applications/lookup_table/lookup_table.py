19a0f77fc76dd89e77aa516377cf8b82fec0981c
ZaStack <stackzpersonal@gmail.com>
2020-07-08 14:03:53 -0700

    Lookup done. Everything is 38 seconds, but at least it's under a minute

--- applications/lookup_table/lookup_table.py
+++ applications/lookup_table/lookup_table.py
@@ -1,26 +1,44 @@
 # Your code here
+import math
+import random
+import time
 
 
-def slowfun_too_slow(x, y):
-    v = math.pow(x, y)
-    v = math.factorial(v)
-    v //= (x + y)
-    v %= 982451653
+# def slowfun_too_slow(x, y):
+#     v = math.pow(x, y)
+#     v = math.factorial(v)
+#     v //= (x + y)
+#     v %= 982451653
 
-    return v
+#     return v
 
-def slowfun(x, y):
+
+
+def slowfun(x, y, cache={}):
     """
     Rewrite slowfun_too_slow() in here so that the program produces the same
     output, but completes quickly instead of taking ages to run.
     """
     # Your code here
-
-
+    
+    
+    if (x, y) in cache:
+        return cache[(x, y)]
+    else:
+        v = math.pow(x, y)
+        v = math.factorial(v)
+        v //= (x + y)
+        v %= 982451653
+        cache[(x, y)] = v
+        return v
 
 # Do not modify below this line!
+start_time = time.time()
 
 for i in range(50000):
     x = random.randrange(2, 14)
     y = random.randrange(3, 6)
     print(f'{i}: {x},{y}: {slowfun(x, y)}')
+
+end_time = time.time()
+print(f'50k took {end_time - start_time} seconds')
